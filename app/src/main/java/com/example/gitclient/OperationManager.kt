94008fa2bb58137d52b0b1a6b79dcbd13913fd4c
Gustavo Quesada <gquesada@konradgroup.com>
2021-06-29 20:33:37 -0600

    Support merge commands

--- app/src/main/java/com/example/gitclient/OperationManager.kt
+++ app/src/main/java/com/example/gitclient/OperationManager.kt
@@ -6,4 +6,12 @@ class OperationManager {
     fun getLog() {
         Log.d("Operation Manager", "Get Log")
     }
+
+    fun rebase() {
+        Log.d("Operation Manager", "Rebase")
+    }
+
+    fun merge() {
+        Log.d("Operation Manager", "Merge")
+    }
 }
